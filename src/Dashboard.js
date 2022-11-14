6928b22c5636d820592c7ff2b0f200e3aab49ab7
MonishacloudS <monishas7122001@gmail.com>
2022-11-14 17:51:13 +0530

    final touches

---
+++ src/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <h1 className="mt-3 text-danger">🙏Welcome to Central Library🎈📘</h1>
+      <img className="mt-3" src="https://png.pngtree.com/background/20211215/original/pngtree-the-background-photography-of-the-empty-library-bookstore-indoors-picture-image_1474812.jpg" alt="" />
+    </div>
+  )
+}
+
+export default Dashboard
